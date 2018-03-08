785a757aeeae7a546564eb9ec03014cddebb923f
draculavlad <450416583@qq.com>
2018-03-08 16:17:40 +0800

    Create Promise.js

---
+++ Promise.js
@@ -0,0 +1,16 @@
+let arr = [{a: 'x'}, {a: 'y'}, {a: 'z'}]
+var sequence = new Promise(() => {})
+const addToSequenceChain = (promiseChain, taskPromise) => {
+  promiseChain.then(taskPromise)
+}
+arr.forEach((v, k) => {
+	console.log('task '+k+' added')
+ 	addToSequenceChain(sequence, new Promise(() => console.log(k)))
+})
+addToSequenceChain(sequence, Promise.all(arr.map(v => console.log(v.a))))
+
+let dataArr = ['a','b','c']
+let funcArr = []
+dataArr.forEach(v => funcArr.push(() => {console.log('.')}))
+console.log(funcArr)
+addToSequenceChain(sequence, Promise.all(funcArr.map(func => func())))
